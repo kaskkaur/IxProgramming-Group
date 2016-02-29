92b0ffcfed740986d72fc24e72487ac61c49b95f
Kaur Kask <kaskkaur@n135-p216.kthopen.kth.se>
2016-02-29 15:16:11 +0100

    yee

--- js/controller/DinnerOverviewController.js
+++ js/controller/DinnerOverviewController.js
@@ -1,8 +1,15 @@
 var DinnerOverviewController = function(view, model) {
 
 
-
+	ConfirmButton.onclick = function() {
 
 	
+
+	stateController.dinnerOverview.container.show();
+	stateController.sideBarView.container.hide();
+	stateController.dishSelectorView.container.hide();
+
+}
+
 
 }
